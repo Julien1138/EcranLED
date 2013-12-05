fc729624f076a441551e5d6f6f9d6c04f7de4f8c
Julien1138 <Julien1138@users.noreply.github.com>
2013-12-05 14:24:16 +0000

    Ajout de la possibilité de modifier une page "Texte1Ligne"

--- AppliWeb/vue/index.php
+++ AppliWeb/vue/index.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
    <head>
-      <title>Configuration de l'écran à LED</title>
+      <?php
+      if (isset($_GET['ajoutimage']))
+      {
+         echo "<title>Ajout d'une image</title>";
+      }
+      else
+      {
+         echo "<title>Configuration de l'écran à LED</title>";
+      }
+      ?>
       <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
    </head>
    <body>
---
+++ AppliWeb/vue/page_modif_texte1.php
@@ -0,0 +1,11 @@
+<p>
+   Texte à afficher :
+   <input type="text" name="Texte" size="10" maxlength="7"
+   <?php
+   if (array_key_exists('Texte', $Page->Parametres()))
+   {
+      echo 'value="' . $Page->Parametres()['Texte'] . '"';
+   }
+   ?>
+   ">
+</p>
