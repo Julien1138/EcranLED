d4da874538812bf94bd48e61db43899c2163cab7
Julien1138 <Julien1138@users.noreply.github.com>
2013-12-05 13:23:17 +0000

    Ajout de la possibilité d'uploader une image

---
+++ AppliWeb/vue/ajout_image.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_FILES['monfichier']) and $_FILES['monfichier']['error'] == 0)
+{
+   $infosfichier = pathinfo($_FILES['monfichier']['name']);
+   $extension_upload = strtolower($infosfichier['extension']);
+   $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+   if (in_array($extension_upload, $extensions_autorisees))
+   {
+      move_uploaded_file($_FILES['monfichier']['tmp_name'], $GLOBALS['DossierImages'] . '/' . basename($_FILES['monfichier']['name']));
+      echo '<p>Chargement du fichier' . $_FILES["monfichier"]["name"] . ': OK</p>';
+   }
+   else
+   {
+      echo '<p>Chargement du fichier' . $_FILES["monfichier"]["name"] . ': NOK</p>';
+   }
+}
+else
+{
+?>
+   <form action="index.php?ajoutimage=1" method="post" enctype="multipart/form-data">
+      <p>
+         Formulaire d'envoi de fichier :<br />
+         <input type="file" name="monfichier" /><br />
+         <input type="submit" value="Envoyer le fichier" />
+      </p>
+   </form>
+<?php
+}
+?>
+<a href="index.php">Retour</a>
