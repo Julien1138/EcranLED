25d078be12c31c27006385de23a6c824e418dddb
Julien1138 <Julien1138@users.noreply.github.com>
2013-12-06 08:38:59 +0000

    Ajout des possibilités d'ajouter et de supprimer une page.

--- AppliWeb/controleur/index.php
+++ AppliWeb/controleur/index.php
@@ -5,6 +5,22 @@ include_once('modele/CConfig.class.php');
 $Config = new CConfig();
 $Config->Charger(sprintf("%s\\config.txt", $GLOBALS["DossierConfig"]));
 
+if (isset($_POST['updatepage']) and isset($_POST['numpage']))  // Mise à jour d'une page
+{
+   $Page=$Config->ListePages()[(int) $_POST['numpage']]; // Récupération de la page
+   include("controleur\\page_modif.php"); // Modification de la page
+}
+
+if (isset($_POST['ajoutpage']))  // Ajout d'une page
+{
+   include("controleur\\page_ajout.php"); // Ajout de la page
+}
+
+if (isset($_GET['supprpage']))  // Suppression d'une page
+{
+   include("controleur\\page_suppression.php"); // Ajout de la page
+}
+
 include('vue/index.php');
 
 ?>
