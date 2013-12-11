1a921388494fa8a3fc86fa2349e8dfccf603abba
Julien1138 <Julien1138@users.noreply.github.com>
2013-12-11 15:21:22 +0000

    Intégration de la bibliothèque ImageMagick Exemple de texte en image dans le main

--- AppliRPi/sources/main.cpp
+++ AppliRPi/sources/main.cpp
@@ -1,17 +1,39 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include "../includes/CImg.h"
 #include <string>
+#include <Magick++.h>
 #include "../includes/CProgramme.h"
-
-//using namespace cimg_library;
 
 int main(int argc,char **argv) 
 {
-   CProgramme Programme;
-   Programme.Charger("config.txt");
+   // CProgramme Programme;
+   // Programme.Charger("/var/www/config/config.txt");
    
+   // Create base image (black image of 192 by 56 pixels)
+   // Magick::Image image( Magick::Geometry(192, 56), Magick::Color("black") );
    
+   // Write the result
+   // image.write("x.png");
+   
+   // Example:
+   Magick::Image my_image( Magick::Geometry(192, 56), Magick::Color("black"));
+   
+   // set the text rendering font (the color is determined by the "current" image setting)
+   my_image.font("Arial");
+   // my_image.boxColor("red");
+   my_image.strokeColor("white");
+   my_image.fillColor("white");
+   my_image.fontPointsize(28);
+   
+   // draw text with different gravity position
+   my_image.annotate("Hello !", Magick::NorthGravity);
+   // my_image.annotate("Super !", Magick::SouthGravity);
+   
+   my_image.draw(Magick::DrawableFont("Arial"));
+   my_image.draw(Magick::DrawableStrokeColor("white"));
+   my_image.draw(Magick::DrawableFillColor(Magick::Color(0, 0, 0, MaxRGB)));
+   my_image.draw(Magick::DrawableTextUnderColor("red"));
+   my_image.draw(Magick::DrawableText(-20,27,"Text Sample with draw"));
+   
+   my_image.write("x.png");
    
    return 0;
 }
