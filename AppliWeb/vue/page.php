cf0dc88da3e4a994b7d995ccb9ef7c856688abf1
Julien1138 <Julien1138@users.noreply.github.com>
2014-01-04 22:20:48 +0000

    Ajout de la page météo. Ajout de la gestion de la couleur des textes. Clarification de l'affichage

--- AppliWeb/vue/page.php
+++ AppliWeb/vue/page.php
@@ -2,7 +2,46 @@
    <?php
    foreach($Page->Parametres() as $nom => $valeur)
    {
-      if ($nom != "Type")
+      if ($nom == "Type")
+      {
+         // On n'affiche pas le type, il est déjà dans le titre.
+      }
+      else if ($nom == "Tempo")
+      {
+         echo sprintf("Durée d'affichage de la page : %s secondes.", $valeur) . '</br>';
+      }
+      else if ($nom == "Format") // Page d'heure
+      {
+         echo sprintf("Format d'affichage de l'heure : %s.", $valeur) . '</br>';
+      }
+      else if ($nom == "Source") // Page d'image
+      {
+         echo sprintf("Nom de l'image affichée : %s", $valeur) . '</br>';
+      }
+      else if ($nom == "Texte") // Page de texte sur une ligne
+      {
+         echo sprintf("Texte affiché : %s", $valeur) . '</br>';
+      }
+      else if ($nom == "Texte1") // Page de texte sur deux lignes
+      {
+         echo sprintf("Texte affiché sur la première ligne : %s", $valeur) . '</br>';
+      }
+      else if ($nom == "Texte2") // Page de texte sur deux lignes
+      {
+         echo sprintf("Texte affiché sur la deuxième ligne : %s", $valeur) . '</br>';
+      }
+      else if ($nom == "Defilement") // Page de texte sur deux lignes
+      {
+         if ($valeur == "oui")
+         {
+            echo "Le texte de la deuxième ligne est défilant</br>";
+         }
+         else
+         {
+            echo "Le texte de la deuxième ligne n'est pas défilant</br>";
+         }
+      }
+      else
       {
          echo sprintf("%s = %s", $nom, $valeur) . '</br>';
       }
