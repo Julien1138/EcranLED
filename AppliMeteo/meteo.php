6e33aa8b2cbade5095c90fb4450bc4511ce58a07
Julien1138 <Julien1138@users.noreply.github.com>
2014-01-05 09:16:24 +0000

    Récupération de la vitesse du vent et de l'humidité de l'air.

--- AppliMeteo/meteo.php
+++ AppliMeteo/meteo.php
@@ -1,12 +1,20 @@
+#!/usr/bin/php
 <?php
-   $FichierMeteo = "data/meteo.txt";
+   $FichierMeteo = "/home/pi/EcranLed/data/meteo.txt";
  
 
 $url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=Gien&format=xml&extra=localObsTime&num_of_days=1&key=jcjx8e5r5h97khnps9m8mrsn";
 $xml = simplexml_load_file($url);
-$tempC = $xml->current_condition->temp_C;
-$File = fopen($FichierMeteo,"w");
+$temp_C = $xml->current_condition->temp_C;
+$humidity = $xml->current_condition->humidity;
+$windspeedKmph = $xml->current_condition->windspeedKmph;
+$url_icon = $xml->current_condition->weatherIconUrl;
 
-fputs($File, sprintf("Temperature=%u\n", $tempC));
+$File = fopen($FichierMeteo,"w");
+fputs($File, sprintf("Temperature=%u\n", $temp_C));
+fputs($File, sprintf("Humidite=%u\n", $humidity));
+fputs($File, sprintf("VitesseVent=%u\n", $windspeedKmph));
 fclose($File);
+
+file_put_contents("/home/pi/EcranLed/data/meteo_icon.png", fopen($url_icon, 'r'))
 ?>
